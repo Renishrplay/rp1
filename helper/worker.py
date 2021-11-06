eb8ceb1137408ea0e56ca8593a13b085c3c75207
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-11-06 16:20:18 +0100

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -234,7 +234,7 @@ async def encod(event):
         xxx = await event.reply("`Downloading...`")
         """ For Force Subscribe Channel"""
          pp = []
-         async for x in event.client.iter_participants("put group username"):
+         async for x in event.client.iter_participants("@rplaymovie"):
             pp.append(x.id)
          if (user.id) not in pp:
             return await xxx.edit(
