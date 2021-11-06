f68310714dd7e451e0d33c4418b52e4cef86e77e
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-11-06 19:51:15 +0100

    Update worker.py

--- helper/worker.py
+++ helper/worker.py
@@ -233,14 +233,14 @@ async def encod(event):
             pass
         xxx = await event.reply("`Downloading...`")
         """ For Force Subscribe Channel"""
-         pp = []
-         async for x in event.client.iter_participants("RPLAY ™ MOVIE"):
-            pp.append(x.id)
-         if (user.id) not in pp:
-            return await xxx.edit(
-                "U Must Subscribe This Channel To Use This Bot",
+        pp = []
+        async for x in event.client.iter_participants("RPLAY ™ MOVIE"):
+           pp.append(x.id)
+        if (user.id) not in pp:
+           return await xxx.edit(
+               "U Must Subscribe This Channel To Use This Bot",
                buttons=[Button.url("JOIN CHANNEL", url="https://t.me/rplaymovie")],
-           )
+            )
         if len(COUNT) > 14 and user.id != OWNER:
             llink = (await event.client(cl(LOG))).link
             return await xxx.edit(
